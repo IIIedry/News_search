9ade2e756875911a57be2e3bd453345e2d8f1b9b
IIIedry <98078659+IIIedry@users.noreply.github.com>
2024-03-30 14:14:22 +0600

    response and newsapi changed

--- NewsSearchApi_1/app/src/main/java/com/example/newssearchapi_1/NewsApi.kt
+++ NewsSearchApi_1/app/src/main/java/com/example/newssearchapi_1/NewsApi.kt
@@ -1,23 +1,51 @@
 package com.example.newssearchapi_1
 
 import androidx.annotation.IntRange
+import com.example.newssearchapi_1.models.Article
+import com.example.newssearchapi_1.models.Response
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Serializer
+import okhttp3.OkHttpClient
 import org.intellij.lang.annotations.Language
+import retrofit2.Retrofit
+import retrofit2.create
 import retrofit2.http.GET
 import retrofit2.http.Query
 import java.util.Date
 
 interface NewsApi{
+    /**
+     * Api details [here](https://newsapi.org/docs/endpoints/everything)
+     */
     @GET("/everything")
-    fun everything(
+    suspend fun everything(
         @Query("q") query: String? = null,
         @Query("from") from: Date? = null,
         @Query("to") to: String? = null,
-        @Query("to") language: List<Language>? = null, //қате болуы мүмкін
+        @Query("language") language: List<Language>? = null,
         @Query("sortBy") sortBy: String? = null,
         @Query("pageSize") @IntRange(from= 0, to = 100) pageSize: Int = 100,
         @Query("page") @IntRange(from = 1) page: Int = 1,
-    )
+    ): Response<Article>
+}
+
+fun NewsApi(
+    baseUrl: String,
+    okHttpClient: OkHttpClient? = null,
+
+): NewsApi{
+    val retrofit = retrofit(baseUrl, okHttpClient)
+    return retrofit.create()
+}
+
+private fun retrofit(
+    baseUrl: String,
+    okHttpClient: OkHttpClient?
+): Retrofit {
+    return Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .run{ if(okHttpClient != null) client(okHttpClient) else this}
+        .
+        .build()
 }
--- NewsSearchApi_1/app/src/main/java/com/example/newssearchapi_1/models/Response.kt
+++ NewsSearchApi_1/app/src/main/java/com/example/newssearchapi_1/models/Response.kt
@@ -7,5 +7,5 @@ import kotlinx.serialization.Serializable
 data class Response<E>(
     @SerialName("status") val status: String,
     @SerialName("totalResults") val totalResults: Int,
-    @SerialName("articles") val articles: List<Article>
+    @SerialName("articles") val articles: List<E>
 )
