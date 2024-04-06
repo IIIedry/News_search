025aeea1824c0467dad96f603eb185f360ffce7f
IIIedry <98078659+IIIedry@users.noreply.github.com>
2024-04-06 13:34:38 +0600

    api module change and adding

--- NewsSearchApi_1/newsapi/build.gradle.kts
+++ NewsSearchApi_1/newsapi/build.gradle.kts
@@ -1,43 +1,32 @@
 plugins {
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("java-library")
+    alias(libs.plugins.jetbrainsKotlinJvm)
+    alias(libs.plugins.kotlinSerialization)
 }
 
-android {
-    namespace = "com.example.newsapi"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
-dependencies {
+dependencies{
+    implementation(libs.retrofit)
+    implementation(libs.kotlinx.coroutines.core)
+    api(libs.kotlinx.serialization.json)
+    implementation(libs.androidx.annotation)
+    implementation(libs.retrofit.converter.kotlinx.serialization)
+    implementation (libs.retrofit.adapters.result)
+    api(libs.okhttp)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+}
+
+dependencies{
+    implementation(libs.retrofit)
+    implementation(libs.kotlinx.coroutines.core)
+    api(libs.kotlinx.serialization.json)
+    implementation(libs.androidx.annotation)
+    implementation(libs.retrofit.converter.kotlinx.serialization)
+    implementation (libs.retrofit.adapters.result)
+    api(libs.okhttp)
+
 }
---
+++ NewsSearchApi_1/newsapi/src/main/java/com/example/newsapi/models/SortBy.kt
@@ -0,0 +1,21 @@
+package com.example.newsapi.models
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+enum class SortBy{
+
+    @SerialName("relevancy")
+    RELEVANCY,
+
+    @SerialName("popularity")
+    POPULARITY,
+
+    @SerialName("publishedAt")
+    PUBLISHED_AT,
+
+
+
+}
